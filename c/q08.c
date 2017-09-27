b031e6237e7d86763dd903de4279aaba4b8aad8c
Leão Izel <eleaoizel@gmail.com>
2017-09-27 08:49:56 -0400

    Feito

--- c/q08.c
+++ c/q08.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-  int a, b, res;
+  int a, b, res, quo;
 
   printf("Informe os números: ");
-  scanf("%d/%d", &a, &b);
+  scanf("%d %d", &a, &b);
 
   res = a - b;
-  while(res > b){
-    res = a - b;
+  while(res >= b){
+    res -= b;
   }
   printf("O resultado é %d\n", res);
   return 0;
